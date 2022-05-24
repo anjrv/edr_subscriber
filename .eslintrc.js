30699c2ac0a97d21f4ae81f344988ce2425e5a48
Anna Jaerving <AnnaJrving@gmail.com>
2022-05-24 11:20:26 +0000

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': ['error', { packageDir: './' }],
+
+    'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
+    'no-restricted-syntax': ['error', 'WithStatement'],
+  },
+};
---
+++ src/app.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+import * as mqtt from 'mqtt';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+  SERVER_URL: url = 'mqtt://localhost:1883',
+} = process.env;
+
+const client = mqtt.connect(url);
+
+client.on('connect', () => {
+  client.subscribe('EDR', (err) => {
+    console.log(err);
+  });
+});
+
+client.on('message', (_topic, message) => {
+  console.log(message.toString());
+  // client.end();
+});
