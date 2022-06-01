863a9872a8464c57911024ce5c3bda3a5651cc00
Anna Jaerving <AnnaJrving@gmail.com>
2022-06-01 16:13:01 +0000

    mongodb insertion function

--- src/app.js
+++ src/app.js
@@ -2,31 +2,58 @@
 import * as mqtt from 'mqtt';
 import dotenv from 'dotenv';
 import pako from 'pako';
+import { MongoClient } from 'mongodb';
 
 dotenv.config();
 
 const {
-  SERVER_URL: url = 'mqtt://localhost:1883',
+  SERVER_URL: mosquitto = 'mqtt://localhost:1883',
+  DATABASE_URL: db = 'http://localhost:27017',
   USERNAME: username = 'username',
   PASSWORD: password = 'password',
 } = process.env;
 
-const clientId = `node${Math.random().toString(16).slice(2)}`;
-const client = mqtt.connect(url, {
-  clientId,
+const mongoClient = new MongoClient(db);
+
+async function run(measurements) {
+  try {
+    await mongoClient.connect();
+
+    const database = mongoClient.db('hvassahraun');
+    const edr = database.collection('edr');
+
+    const options = { ordered: true };
+    const result = await edr.insertMany(measurements, options);
+    console.log(`${result.insertedCount} measurements were inserted`);
+  } finally {
+    await mongoClient.close();
+  }
+}
+
+const mqttClientId = `node${Math.random().toString(16).slice(2)}`;
+const mqttClient = mqtt.connect(mosquitto, {
+  mqttClientId,
   username,
   password,
 });
 
-client.on('connect', () => {
-  client.subscribe('EDR', (err) => {
+mqttClient.on('connect', () => {
+  mqttClient.subscribe('EDR', (err) => {
     if (err) console.err(err);
   });
 });
 
-client.on('message', (_topic, message) => {
-  const data = JSON.parse(pako.ungzip(message, { to: 'string' }));
-  console.log(data);
-  // Add repeat tags back into each measurement
-  // Bulk insert into db ( could also collect bigger bulks )
+mqttClient.on('message', (_topic, message) => {
+  const msg = JSON.parse(pako.ungzip(message, { to: 'string' }));
+  const {
+    brand, manufacturer, model, id, version, data,
+  } = msg;
+
+  // Unroll user information back into every measurement
+  const entry = data.map((obj) => Object.assign(obj, {
+    brand, manufacturer, model, id, version,
+  }));
+
+  console.log(entry[0]);
+  // run(entry).catch(console.dir);
 });
